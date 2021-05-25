9de6ee7ce1dfe6afa611a0d34a9d51762a592f03
abhay9236 <abhay9236@gmail.com>
2021-05-25 14:52:13 +0530

    new files added

---
+++ CodeAnalysis/CodeAnalysis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "a", "b", "bc", "d" };
+            var names2 = names.Where(n => n.StartsWith("b"));
+            Console.ReadLine();
+        }
+    }
+}
